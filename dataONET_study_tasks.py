14c9e689039609bfecc0c8f18d57f5f6ab6df77b
Aniket Baksy <anikbak@stanford.edu>
2022-08-23 21:49:07 +0530

    edits

--- dataONET_study_tasks.py
+++ dataONET_study_tasks.py
@@ -3,13 +3,19 @@
 ########################################################################################################
 
 import numpy as np, pandas as pd 
+level = 4 # 0
 
 # Load Data
-cols = ['O*NET-SOC Code', 'Task', 'VerbNouns_dobj',
+if level == 0:
+    cols = ['O*NET-SOC Code', 'Task', 'VerbNouns_dobj',
        'Occupation_VerbNouns', 'OrigTaskIndex', 'Task ID', 'Task Type',
        'Incumbents Responding', 'Domain Source']
-
-df = pd.read_csv("data/data_onet_task_verbnoun_pairs.csv",usecols=cols)
+    df = pd.read_csv("data/data_onet_task_verbnoun_pairs.csv",usecols=cols)
+elif level == 4:
+    cols = ['O*NET-SOC Code', 'Task', 'VerbNouns_dobj_NounLevel_4',
+       'Occupation_VerbNouns', 'OrigTaskIndex', 'Task ID', 'Task Type',
+       'Incumbents Responding', 'Domain Source']
+    df = pd.read_csv("data/data_onet_task_nounlevel4_verbnoun_pairs.csv")
 
 # Construct Importance Measures for Occupational Tasks
 colsImp = ['O*NET-SOC Code', 'Task ID', 'Scale ID', 'Category', 'Data Value', 'N']
@@ -36,11 +42,17 @@ dfImp_ = dfImpRT.copy()
 dfImp_ = dfImp_.merge(dfImpIM,on=['O*NET-SOC Code', 'Task ID'],how='outer')
 dfImp_ = dfImp_.merge(dfImpFT,on=['O*NET-SOC Code', 'Task ID'],how='outer')
 
+# Construct Importance Indices
+dfImp_ = dfImp_.set_index(['O*NET-SOC Code','Task ID'])
+dfImp_[['Weight RT','Weight IM']] = dfImp_.groupby(['O*NET-SOC Code','Task ID'])[['Data Value RT','Data Value IM']].mean()
+dfImp_ = dfImp_.reset_index() 
+
 # Add Occupation Titles and Importance Measures to Occupational Tasks
 dfNam = pd.read_csv("data_raw/onet/Occupation Data.txt",sep='\t')
 
 df = df.merge(dfImp_,on=['O*NET-SOC Code','Task ID'],how='outer',indicator='_m_occRatings')
 df = df.merge(dfNam,on=['O*NET-SOC Code'],how='outer')
+df = df.drop(columns='Unnamed: 0')
 
 # Export 
 df.to_csv('data/data_onet_weights.csv')
